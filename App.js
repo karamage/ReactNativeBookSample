d98f8d6b5cb51d0317e0ff188382a2fd90234969
Masaaki Kakimoto <karamage.umauma@gmail.com>
2018-08-23 19:44:46 +0900

    Hogeコンポーネントを読み込んで表示してみる

--- App.js
+++ App.js
@@ -17,14 +17,22 @@ const instructions = Platform.select({
 });
 
 type Props = {};
+
+import Hoge from './Hoge'
+
 export default class App extends Component<Props> {
   render() {
     return (
+      <View style={styles.container}>
+        <Hoge/>
+      </View>
+      /*
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
+      */
     );
   }
 }
---
+++ Hoge.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import {Text} from 'react-native';
+
+export default class Hoge extends Component {
+  render() {
+    return (
+      <Text>hoge</Text>
+    )
+  }
+}
+
