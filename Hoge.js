5ad6c75bb773126e6ae14536ffa18d1cfd75d337
Masaaki Kakimoto <karamage.umauma@gmail.com>
2018-08-24 12:40:48 +0900

    Hogeコンポーネントにメソッド

--- Hoge.js
+++ Hoge.js
@@ -9,7 +9,10 @@ export default class Hoge extends Component {
     }
   }
   componentDidMount() {
-    if (!this.props.isShow) {
+    this.updateText(this.props.isShow)
+  }
+  updateText(isShow) {
+    if (!isShow) {
       this.setState({text:"非表示です"})
     }
   }
